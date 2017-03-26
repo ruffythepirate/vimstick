919041cd42787d7f8a694cc1c8283dc22e9bb9ff
ruffy <johan.ellfries@gmail.com>
2017-03-26 21:20:38 +0200

    add support for different input handlers.

--- src/app/terminal.js
+++ src/app/terminal.js
@@ -14,24 +14,27 @@ function nl2br(txt){ /* helper, textarea return \n not <br /> */
     return txt.replace(/\n/g, "<br />");
 }
 
-function writeit(from, e){ /* the magic starts here, this function requires the element from which the value is extracted and an event object */
-    e = e || window.event; /* window.event fix for browser compatibility */
-    var w = $("writer"); /* get the place to write */
-    var tw = from.value; /* get the value of the textarea */
-    w.innerHTML = nl2br(tw); /* convert newlines to breaks and append the returned value to the content area */
-}
+window.inputHandler = inputInputHandler();
 
-function moveIt(count, e){ 
-    e = e || window.event; 
-    var keycode = e.keyCode || e.which;
-    if(keycode == 37 && parseInt(cursor.style.left) >= (0-((count-1)*10))){ 
-        cursor.style.left = parseInt(cursor.style.left) - 10 + "px"; 
-    } else if(keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0){ 
-        cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
+function inputInputHandler() {
+    const self = {};
+    self.writeIt = function(from, e) {
+        e = e || window.event; /* window.event fix for browser compatibility */
+        var w = $("writer"); /* get the place to write */
+        var tw = from.value; /* get the value of the textarea */
+        w.innerHTML = nl2br(tw); /* convert newlines to breaks and append the returned value to the content area */
     }
 
-}
-
-            function alert(txt){ // for debugging
-            console.log(txt); // works only with firebug
+    self.moveIt = function(from, e) {
+        e = e || window.event; 
+        var keycode = e.keyCode || e.which;
+        if(keycode == 37 && parseInt(cursor.style.left) >= (0-((count-1)*10))){ 
+            cursor.style.left = parseInt(cursor.style.left) - 10 + "px"; 
+        } else if(keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0){ 
+            cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
         }
+
+    }
+
+    return self;
+}
