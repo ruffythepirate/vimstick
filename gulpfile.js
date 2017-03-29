8e366a91f875df2cabf345e9d4ef7b45e7bbeea0
ruffy <johan.ellfries@gmail.com>
2017-03-29 21:07:50 +0200

    start setting up gulp.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// Gulp.js configuration
+var
+  // modules
+  gulp = require('gulp'),
+
+  // development mode?
+  devBuild = (process.env.NODE_ENV !== 'production'),
+
+  // folders
+  folder = {
+    src: 'src/',
+    build: 'build/'
+  };
--- specs/terminal/inputTree.spec.js
+++ specs/terminal/inputTree.spec.js
@@ -4,12 +4,26 @@ const chai = require('chai');
 
 const expect = chai.expect;
 
-describe('inputTree.inputKey', function () {
-  it('calls default command, when given a key that was not initialized', function (){
-    cut.initializeTree(null, {id: 'input'});
+describe('inputTree.inputKey', function() {
+  it('calls default command, when given a key that was not initialized', function() {
+    cut.initializeTree(null, {
+      id: 'input'
+    });
     const response = cut.inputKey({});
 
     expect(response.id).to.be.equal('input');
+  });
+
+  it('maps a keystroke to one key', function() {
+    cut.initializeTree([{id:'custom', key: 'a'}]);
+
+    const response = cut.inputKey({ key: 'a' });
+
+    expect(response.id).to.be.equal('custom');
+  });
+
+
+  it('can map two keystrokes to two keys', function() {
 
   });
 
